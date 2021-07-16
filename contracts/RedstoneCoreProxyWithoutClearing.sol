bbaf6a27daf08c9b5ffdc57509b66ed237ccb4d9
hatskier <suvorovalex1997@gmail.com>
2021-07-16 09:01:44 +0200

    contracts version without clearing implemented

---
+++ contracts/RedstoneCoreProxyWithoutClearing.sol
@@ -0,0 +1,75 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.2;
+
+import "./BytesLib.sol";
+import 'hardhat/console.sol';
+import './PriceFeed.sol';
+import "@openzeppelin/contracts/proxy/Proxy.sol";
+
+abstract contract RedstoneCoreProxyWithoutClearing is Proxy {
+    using BytesLib for bytes;
+
+    bytes32 constant MARKER = keccak256("Redstone.version.0.0.1");
+
+    /**
+     * @dev Delegates the current call to `implementation`.
+     *
+     * It extracts the pricing data and keep it in the storage of priceFeed contract. 
+     * Then it forwards the call to the implementation recording the results. 
+     * As the last step it cleans the data from priceFeed to save on gas fees
+     * and forwards the results to the caller.
+     */
+    function _delegate(address implementation) internal override {
+
+        // Check if transaction contains RedStone marker
+        bool isTxWithPricing = false;
+        if (msg.data.length > 32) {
+            isTxWithPricing = msg.data.toBytes32(msg.data.length - 32) == MARKER;
+        }
+
+
+        // Register price data
+        bytes memory priceData;
+        uint16 priceDataLen;
+        if (isTxWithPricing) {
+            priceDataLen = msg.data.toUint16(msg.data.length - 34);
+            priceData = msg.data.slice(msg.data.length - priceDataLen - 34, priceDataLen);
+            (bool success,) = _priceFeed().call(priceData);
+            require(success, "Error setting price data");
+        }
+
+        // Copied from openzeppelin github repo
+        // https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/proxy/Proxy.sol
+        assembly {
+            // Copy msg.data. We take full control of memory in this inline assembly
+            // block because it will not return to Solidity code. We overwrite the
+            // Solidity scratch pad at memory position 0.
+            calldatacopy(0, 0, calldatasize())
+
+            // Call the implementation.
+            // out and outsize are 0 because we don't know the size yet.
+            let result := delegatecall(gas(), implementation, 0, calldatasize(), 0, 0)
+
+            // Copy the returned data.
+            returndatacopy(0, 0, returndatasize())
+
+            switch result
+            // delegatecall returns 0 on error.
+            case 0 {
+                revert(0, returndatasize())
+            }
+            default {
+                return(0, returndatasize())
+            }
+        }
+    }
+
+
+    /**
+     * @dev This is a virtual function that should be overriden
+     * to return the address of the contract which keeps the prices on-chain
+     */
+    function _priceFeed() internal view virtual returns (address);
+
+}
---
+++ test/contracts/DefiWithRedstonePriceFeedWithoutClearing.test.ts
@@ -0,0 +1,109 @@
+import { ethers } from "hardhat";
+import chai from "chai";
+import { solidity } from "ethereum-waffle";
+import { MockDefi } from "../../typechain/MockDefi";
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
+import {PriceVerifier} from "../../typechain/PriceVerifier";
+import redstone from 'redstone-api';
+const { wrapContract } = require("../../utils/contract-wrapper");
+
+import { PriceFeedWithoutClearing } from "../../typechain/PriceFeedWithoutClearing";
+chai.use(solidity);
+
+const { expect } = chai;
+
+const toBytes32 = ethers.utils.formatBytes32String;
+const serialized = function (x: number): number {
+    return x * 10**8;
+};
+
+describe("MockDefi with Proxy contract and pricing Data", function() {
+
+    const REDSTONE_STOCKS_PROVIDER = "Yba8IVc_01bFxutKNJAZ7CmTD5AVi2GcWXf1NajPAsc";
+    const REDSTONE_STOCKS_PROVIDER_ADDRESS = "0x926E370fD53c23f8B71ad2B3217b227E41A92b12";
+
+
+    let owner: SignerWithAddress;
+    let admin: SignerWithAddress;
+    let defi: MockDefi;
+    let priceFeed: PriceFeedWithoutClearing;
+    let verifier: PriceVerifier;
+    let apiPrices: any;
+
+    async function loadApiPrices() {
+        apiPrices = await redstone.getAllPrices({
+            provider: "redstone-stocks",
+        });
+    }
+
+    it("Deployment should have zero balance", async function() {
+        [owner, admin] = await ethers.getSigners();
+
+        const Defi = await ethers.getContractFactory("MockDefi");
+        const Proxy = await ethers.getContractFactory("RedstoneUpgradeableProxyWithoutClearing");
+        const PriceFeedWithoutClearing = await ethers.getContractFactory("PriceFeedWithoutClearing");
+        const Verifier = await ethers.getContractFactory("PriceVerifier");
+
+        verifier = (await Verifier.deploy()) as PriceVerifier;
+        priceFeed = (await PriceFeedWithoutClearing.deploy(verifier.address, 5 * 60)) as PriceFeedWithoutClearing;
+        await priceFeed.authorizeSigner(REDSTONE_STOCKS_PROVIDER_ADDRESS);
+        // console.log("Authorized: ", REDSTONE_STOCKS_PROVIDER_ADDRESS);
+
+        defi = (await Defi.deploy()) as MockDefi;
+
+        // console.log("Defi address: " + defi.address);
+        const proxy = await Proxy.deploy(defi.address, priceFeed.address, admin.address, []);
+
+        defi = (await Defi.attach(proxy.address)) as MockDefi;
+        await defi.initialize(priceFeed.address);
+
+        //defi = defi.connect(signer);
+
+    });
+
+
+    it("Should deposit - write no pricing info multi", async function() {
+
+        defi = wrapContract(defi, REDSTONE_STOCKS_PROVIDER);
+
+        await defi.deposit(toBytes32("GOOG"), 1);
+        await defi.deposit(toBytes32("IBM"), 1);
+
+    });
+
+
+    it("Should inject correct prices from API multi", async function() {
+
+        await loadApiPrices();
+
+        expect(await defi.currentValueOf(owner.address, toBytes32("GOOG")))
+            .to.be.equal(serialized(apiPrices['GOOG'].value).toFixed(0));
+        expect(await defi.currentValueOf(owner.address, toBytes32("IBM")))
+            .to.be.equal(serialized(apiPrices['IBM'].value).toFixed(0));
+
+    });
+
+
+    it("Should deposit - write no pricing info single", async function() {
+
+        defi = wrapContract(defi, REDSTONE_STOCKS_PROVIDER, "FB");
+
+        await Promise.all([
+            defi.deposit(toBytes32("FB"), 1),
+            loadApiPrices(),
+        ]);
+
+    });
+
+
+    it("Should inject correct prices from API single", async function() {
+
+        // const apiPrices = await redstone.getAllPrices({provider:"redstone-stocks"});
+
+        expect(await defi.currentValueOf(owner.address, toBytes32("FB")))
+            .to.be.equal(serialized(apiPrices['FB'].value).toFixed(0));
+
+    });
+   
+
+});
