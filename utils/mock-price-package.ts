a9ba91cc0860d2206446bbf3ffa56e8decddff97
ppedziwiatr <peter@redstone.finance>
2021-08-16 21:58:09 +0200

    some mockin' bullshit

---
+++ utils/mock-price-package.ts
@@ -0,0 +1,11 @@
+import {PricePackage} from "redstone-node/dist/src/types";
+
+export function mockPricePackage(forTime: number): PricePackage {
+  return {
+    prices: [
+      {symbol: "ETH", value: 10},
+      {symbol: "AVAX", value: 5}
+    ],
+    timestamp: forTime - 1000
+  }
+}
