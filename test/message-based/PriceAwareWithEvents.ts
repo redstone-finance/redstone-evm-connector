8c727dd7a9c0432a5c419a1c965a840615b236a9
hatskier <suvorovalex1997@gmail.com>
2022-05-19 21:29:07 +0200

    chore: few fixes after self-review

--- test/message-based/PriceAwareWithEvents.ts
+++ test/message-based/PriceAwareWithEvents.ts
@@ -4,26 +4,28 @@ import { SampleWithEvents } from "../../typechain/SampleWithEvents";
 import { expect } from "chai";
 import { Event } from "ethers";
 
-describe("Example with events", function () {
+describe("Sample with events", function () {
   let sampleContract: SampleWithEvents;
 
   beforeEach(async () => {
-    const ExampleContractWithEvents = await ethers.getContractFactory("SampleWithEvents");
-    sampleContract = await ExampleContractWithEvents.deploy() as SampleWithEvents;
+    const SampleWithEvents = await ethers.getContractFactory("SampleWithEvents");
+    sampleContract = await SampleWithEvents.deploy() as SampleWithEvents;
   });
 
   it("Test events with contract wrapping", async function () {
+    // Wrapping the contract instnace
     sampleContract = WrapperBuilder
       .wrapLite(sampleContract)
       .usingPriceFeed("redstone-avalanche-prod", { asset: "ETH" });
 
+    // Sending tx
     const tx = await sampleContract.emitEventWithLatestEthPrice();
     const receipt = await tx.wait();
+    const event: Event = receipt.events![0];
 
+    // Receipt should have parsed events
     expect(receipt.events!.length).to.be.equal(1);
-    const event: any = receipt.events![0];
-    expect(event.args._ethPrice.toNumber()).to.be.gt(0);
+    expect(event.args!._ethPrice!.toNumber()).to.be.gt(0);
     expect(event.event).to.be.equal("priceUpdated");
-
   });
 });
